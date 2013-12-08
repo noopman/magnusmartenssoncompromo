bb7b022cb43164a5310ecdb0b66f6ea5380bb3e9
Magnus Mårtensson <magnus@magnusmartensson.com>
2013-12-08 20:16:10 +0100

    Creating the site for my demos

---
+++ CommercialDemo/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+// ReSharper disable once CheckNamespace
+namespace CommercialDemo
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
---
+++ CommercialDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CommercialDemo.MvcApplication" Language="C#" %>
